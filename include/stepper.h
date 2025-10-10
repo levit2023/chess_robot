35fbc08e9d537d2ebec312e7c14440513af5f839
levit2023 <levit2023@gmail.com>
2025-10-10 01:38:13 -0400

    feat: two motor support

--- include/stepper.h
+++ include/stepper.h
@@ -5,7 +5,7 @@ extern bool step_done;
 
 void stepper_init_pins();
 void stepper_init_timer();
-void stepper_steps(bool cw, int steps);
+void stepper_steps(bool left_fw, bool right_fw, int steps);
 void stepper_isr();
 
 #endif
--- src/main.c
+++ src/main.c
@@ -15,11 +15,52 @@ int main()
     // test code
     stepper_init_pins();
     stepper_init_timer();
-    stepper_steps(true, 512); // 4096 is one revolution
+
+    // pending: forward, left_turn, right_turn movement functions
+    stepper_steps(true, true, 2000); // 4096 is one revolution
     while (!step_done) {
         tight_loop_contents();
     }
-    stepper_steps(false, 512);
+
+    stepper_steps(true, false, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(true, true, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(false, true, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(true, true, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(false, true, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(true, true, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(true, false, 2000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
+
+    stepper_steps(false, false, 4000);
+    while (!step_done) {
+        tight_loop_contents();
+    }
 
     for(;;);
     return 0;
