5b7d97620f508519bf5850b25b961a6e1f67841a
E_Chang_eprog <120538292+arduinoIsFun@users.noreply.github.com>
2025-10-14 16:45:08 -0400

    Merged with robot_IR

--- src/main.c
+++ src/main.c
@@ -1,29 +1,29 @@
-#include <utils.h>
-#include <movement.h>
-//0x(W/B)(piece) ex: 0x00 is wite castle
-#define IDENT piece //IR rx address
+// #include <utils.h>
+// #include <movement.h>
+// //0x(W/B)(piece) ex: 0x00 is wite castle
+// #define IDENT piece //IR rx address
 
-int main()
-{
-    move_state_t ms;
-    chess_piece_t piece = W_KNIGHT_B; //starting position
-    move_init(&ms, piece);
-    // Configures our microcontroller to 
-    // communicate over UART through the TX/RX pins
-    stdio_init_all();
+// int main()
+// {
+//     move_state_t ms;
+//     chess_piece_t piece = W_KNIGHT_B; //starting position
+//     move_init(&ms, piece);
+//     // Configures our microcontroller to 
+//     // communicate over UART through the TX/RX pins
+//     stdio_init_all();
 
-//     // stepper inits
-//     stepper_init_pins();
-//     stepper_init_timer();
+// //     // stepper inits
+// //     stepper_init_pins();
+// //     stepper_init_timer();
 
-    move_to(&ms, 2,2);
-    // move_to(&ms, 0,0);
-    // move_to(&ms, 2,2);
-    // move_to(&ms, 2,0);
-    // move_to(&ms, 2,2);
-    // move_to(&ms, 3,2);
-    // move_to(&ms, 0,2);
+//     move_to(&ms, 2,2);
+//     // move_to(&ms, 0,0);
+//     // move_to(&ms, 2,2);
+//     // move_to(&ms, 2,0);
+//     // move_to(&ms, 2,2);
+//     // move_to(&ms, 3,2);
+//     // move_to(&ms, 0,2);
 
-// //     for(;;);
-// //     return 0;
+// // //     for(;;);
+// // //     return 0;
 // }
