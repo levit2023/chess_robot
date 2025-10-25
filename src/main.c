ea1a589b81c1692f14679ee1b299c563f2f144d9
Emmanuel Chang <120538292+arduinoIsFun@users.noreply.github.com>
2025-10-25 15:48:04 -0400

    Recieve IR working

--- src/main.c
+++ src/main.c
@@ -4,9 +4,9 @@
 //0x(W/B)(piece) ex: 0x00 is wite castle
 #define IDENT piece //IR rx address
 
-PIO pio = pio0;                                 // choose which PIO block to use (RP2040 has two: pio0 and pio1)
-uint tx_gpio = 18;                              // choose which GPIO pin is connected to the IR LED
-uint rx_gpio = 30; 
+PIO pio = pio0;          // choose which PIO block to use (RP2040 has two: pio0 and pio1)
+uint tx_gpio = 18;       // choose which GPIO pin is connected to the IR LED
+uint rx_gpio = 12; 
 
 int main()
 {
@@ -21,13 +21,21 @@ int main()
     int rx_sm = nec_rx_init(pio, rx_gpio); 
     // Configures our microcontroller to 
     // communicate over UART through the TX/RX pins
-    while(move_data.piece != piece){
+    while(true){
         rx_data = data_recieve(rx_sm, pio);
         move_data = decode_and_check(rx_data);
-        sleep_ms(100);
+        if(move_data.data_valid){
+            if(move_data.piece == 0x08){
+                printf("\r\tRecieved: %02x", move_data.piece);
+                fflush(stdout);
+            }
+            else{
+                printf("\r\tPass Parity Error\n: %02x", move_data.piece);
+            }
+        }
+        sleep_ms(200);
     }
-    printf("\r\tRecieved: %02x", move_data.piece);
-    fflush(stdout);
+
     // stepper inits
     // stepper_init_pins();
     // stepper_init_timer();
